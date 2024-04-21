dc3c2612e8469d698be390742cbce87a0060eb0f
Subrata Mondal <subratasubha2@gmail.com>
2024-04-21 11:03:38 +0530

    ✅ Integrate Logout feature with django and alert message using bootstrap5.

--- crm/views.py
+++ crm/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import (
+    render,
+    redirect,
+    HttpResponse,
+    HttpResponsePermanentRedirect,
+    HttpResponseRedirect,
+)
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 # Create your views here.
-def home(request):
+
+
+# The Login is implemented in the 'home' view itself
+def home(
+    request,
+) -> HttpResponse | HttpResponsePermanentRedirect | HttpResponseRedirect:
     # Check if the user is logging in i.e sending POST Request
     if request.method == "POST":
         username = request.POST["username"]
@@ -17,14 +28,21 @@ def home(request):
             messages.success(request=request, message="✅ You have been logged in!!!")
             return redirect(to="home")
         else:
-            messages.error(request=request, message="❌ Invalid Credentials. Please provide correct details.")
+            messages.error(
+                request=request,
+                message="❌ Invalid Credentials. Please provide correct details.",
+            )
             return redirect(to="home")
     else:
         # If not logging in means already an user, send them to home
         return render(request=request, template_name="home.html", context={})
 
+
 # def login_user(request):
 #     pass
 
-# def logout_user(request):
-#     pass
+
+def logout_user(request) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
+    logout(request=request)
+    messages.success(request=request, message="✅ You have been logged out.")
+    return redirect(to="home")
