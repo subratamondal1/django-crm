4a4ecce9d02a81f2a45581dd32d1571555e4e5f8
Subrata Mondal <subratasubha2@gmail.com>
2024-05-08 17:46:58 +0530

    Modified .sh file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,8 @@
+# Upgrade pip
+python3.9 -m ensurepip --upgrade
+
+# Install project dependencies
 python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic
